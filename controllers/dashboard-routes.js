0f0f6c060b5365f7c0b8384f816b271ea1e59afa
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-09-12 16:36:01 -0400

    fixed home handlebars showing just content and styled correctly also formatDate added when adding date too

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -24,9 +24,33 @@ router.get("/", async (req, res) => {
     res.status(400).json(err);
   }
 });
+
 // GET /dashboard/newPost
 router.get("/newPost", async (req, res) => {
   res.render("create-post", { logged_in: req.session.logged_in });
+});
+
+// GET /dashboard/editPost
+router.get("/editPost", async (req, res) => {
+  console.log("edit post now, req: ", req.body.id);
+  try {
+    const postsData = await Post.findOne({
+      where: {
+        id: req.session.id,
+      },
+      include: {
+        model: User,
+        attributes: ["id", "username"],
+      },
+    });
+    //console.log(postsData);
+    const posts = postsData.map((post) => post.get({ plain: true }));
+    //console.table(posts);
+    res.render("edit-post", { posts, logged_in: req.session.logged_in });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
